daf40010dad889c674a4f5a821c3d75615642a3b
YuYoHan <zxzz8014@naver.com>
2023-05-02 19:20:57 +0900

    [Feat] SignUp 기능 구현

---
+++ src/main/java/com/example/board2/DTO/SignUpResponseDto.java
@@ -0,0 +1,13 @@
+package com.example.board2.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignUpResponseDto {
+    private String token;
+    private int exprTime;
+}
