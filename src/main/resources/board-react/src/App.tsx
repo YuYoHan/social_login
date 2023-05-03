d8b93d4df64fa1c77b09d579ffdad93a3b9e657f
YuYoHan <zxzz8014@naver.com>
2023-05-03 11:41:07 +0900

    [Feat] signUp 기능 구현

--- src/main/resources/board-react/src/App.tsx
+++ src/main/resources/board-react/src/App.tsx
@@ -26,7 +26,6 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>{connection}</p>
             </header>
             <SignUp />
         </div>
