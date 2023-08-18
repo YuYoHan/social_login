b0978d5103234f7b2b9dc4f40f9b9018ddd852b3
YuYoHan <zxzz8014@naver.com>
2023-08-18 12:06:18 +0900

    [Feat] auditing 기능 구현

---
+++ src/main/java/com/example/board3/entity/base/BaseTimeEntity.java
@@ -0,0 +1,30 @@
+package com.example.board3.entity.base;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@EntityListeners(value = {AuditingEntityListener.class})
+@MappedSuperclass
+@Getter
+@Setter
+@ToString
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime regTime;
+
+    @LastModifiedDate
+    private LocalDateTime updateTime;
+
+}
