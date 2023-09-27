8f83ce6e82ac3d42c47c56a3b2f0a92ef7c563ae
YuYoHan <zxzz8014@naver.com>
2023-09-27 16:28:06 +0900

    [Study] OAtuh2 JWT 반환

---
+++ src/main/java/com/example/social/entity/MemberEntity.java
@@ -0,0 +1,46 @@
+package com.example.social.entity;
+
+import com.example.social.domain.Role;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity(name = "member")
+@Table
+@Getter
+@NoArgsConstructor
+public class MemberEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String email;
+    private String password;
+    private String userName;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private String provider;
+    private String providerId;
+
+    @Builder
+    public MemberEntity(Long id,
+                        String email,
+                        String password,
+                        String userName,
+                        Role role,
+                        String provider,
+                        String providerId) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.userName = userName;
+        this.role = role;
+        this.provider = provider;
+        this.providerId = providerId;
+    }
+}
