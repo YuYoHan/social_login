757e3d1ae45df9a776bdad7c5e597bb3f1031c48
YuYoHan <zxzz8014@naver.com>
2023-09-27 16:28:31 +0900

    [Study] OAtuh2 JWT 반환

---
+++ src/main/java/com/example/social/repository/MemberRepositroy.java
@@ -0,0 +1,10 @@
+package com.example.social.repository;
+
+import com.example.social.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepositroy extends JpaRepository<MemberEntity, Long> {
+    MemberEntity findByEmail(String email);
+}
